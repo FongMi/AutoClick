945f516dcc0ba35b0fd61462cabfd07e1867e2ce
FongMi <b8892778@gmail.com>
2020-12-15 16:11:45 +0800

    Add wifi reset when boot

--- AutoClick/app/src/main/java/com/fongmi/android/autoclick/receiver/BootReceiver.java
+++ AutoClick/app/src/main/java/com/fongmi/android/autoclick/receiver/BootReceiver.java
@@ -3,6 +3,7 @@ package com.fongmi.android.autoclick.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.WifiManager;
 
 import com.fongmi.android.autoclick.db.AppDatabase;
 import com.fongmi.android.autoclick.model.Target;
@@ -15,11 +16,18 @@ public class BootReceiver extends BroadcastReceiver {
 	public void onReceive(Context context, Intent intent) {
 		List<Target> items = AppDatabase.get().getTargetDao().getBoot();
 		for (Target item : items) openApp(context, item.getPack());
+		resetWifi(context);
 	}
 
 	private void openApp(Context context, String pack) {
 		Intent intent = context.getPackageManager().getLaunchIntentForPackage(pack);
 		intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 		context.startActivity(intent);
+	}
+
+	private void resetWifi(Context context) {
+		WifiManager wifi = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+		wifi.setWifiEnabled(false);
+		wifi.setWifiEnabled(true);
 	}
 }
