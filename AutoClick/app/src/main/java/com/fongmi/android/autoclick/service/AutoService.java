148184051f5a13f69b51932724326fbd638e2dd7
FongMi <b8892778@gmail.com>
2020-12-15 15:19:41 +0800

    Fix trigger for system dialog

--- AutoClick/app/src/main/java/com/fongmi/android/autoclick/service/AutoService.java
+++ AutoClick/app/src/main/java/com/fongmi/android/autoclick/service/AutoService.java
@@ -1,35 +1,35 @@
 package com.fongmi.android.autoclick.service;
 
 import android.accessibilityservice.AccessibilityService;
-import android.text.TextUtils;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 
 import com.fongmi.android.autoclick.db.AppDatabase;
 import com.fongmi.android.autoclick.model.Target;
 
+import java.util.Collections;
 import java.util.List;
 
 public class AutoService extends AccessibilityService {
 
 	@Override
 	public void onAccessibilityEvent(AccessibilityEvent event) {
-		for (Target item : AppDatabase.get().getTargetDao().getAll()) clickWord(item);
+		for (Target item : AppDatabase.get().getTargetDao().getAll()) onClick(item);
 	}
 
-	private boolean shouldClick(AccessibilityNodeInfo info, Target item) {
-		return info.getPackageName() != null && (info.getPackageName().equals(item.getPack()) || info.getPackageName().equals("com.android.systemui"));
-	}
-
-	private void clickWord(Target item) {
-		try {
-			if (getRootInActiveWindow() == null) return;
-			if (TextUtils.isEmpty(item.getKeyword())) return;
-			List<AccessibilityNodeInfo> list = getRootInActiveWindow().findAccessibilityNodeInfosByText(item.getKeyword());
-			for (AccessibilityNodeInfo info : list) if (shouldClick(info, item)) info.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-		} catch (Exception e) {
-			e.printStackTrace();
+	private void onClick(Target item) {
+		for (AccessibilityNodeInfo info : find(item.getKeyword())) {
+			if (info.getPackageName().equals(item.getPack())) {
+				info.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+			} else if (info.getPackageName().equals("com.android.systemui") && find(item.getName()).size() > 0) {
+				info.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+			}
 		}
+	}
+
+	private List<AccessibilityNodeInfo> find(String text) {
+		if (getRootInActiveWindow() == null) return Collections.emptyList();
+		return getRootInActiveWindow().findAccessibilityNodeInfosByText(text);
 	}
 
 	@Override
